175960ee034b3312d92e0cae96c2c8499b276d17
m-yoshida <m-yoshida@shinsankenkai.com>
2023-04-15 02:01:34 +0000

    created homeCareClinics

--- app/Http/Controllers/HomeCareClinicController.php
+++ app/Http/Controllers/HomeCareClinicController.php
@@ -2,9 +2,36 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\HomeCareClinicRequest;
+use App\Models\HomeCareClinic;
 
 class HomeCareClinicController extends Controller
 {
-    //
+    public function index()
+    {
+        return HomeCareClinic::all();
+    }
+
+    public function store(HomeCareClinicRequest $request)
+    {
+        $homeCareClinic = HomeCareClinic::create($request->validated());
+        return $homeCareClinic;
+    }
+
+    public function show(HomeCareClinic $homeCareClinic)
+    {
+        return $homeCareClinic;
+    }
+
+    public function update(HomeCareClinicRequest $request, HomeCareClinic $homeCareClinic)
+    {
+        $homeCareClinic->update($request->validated());
+        return $homeCareClinic;
+    }
+
+    public function destroy(HomeCareClinic $homeCareClinic)
+    {
+        $homeCareClinic->delete();
+        return response()->json(['message' => 'Home care clinic deleted']);
+    }
 }
--- database/migrations/2023_04_15_002905_create_home_care_clinics_table.php
+++ database/migrations/2023_04_15_002905_create_home_care_clinics_table.php
@@ -4,24 +4,25 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateHomeCareClinicsTable extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
+    public function up()
     {
         Schema::create('home_care_clinics', function (Blueprint $table) {
             $table->id();
+            $table->string('name')->unique();
+            $table->string('abbreviation')->nullable();
+            $table->string('postal_code')->nullable();
+            $table->text('address')->nullable();
+            $table->string('phone_number')->nullable();
+            $table->string('fax_number')->nullable();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('home_care_clinics');
     }
-};
+}
