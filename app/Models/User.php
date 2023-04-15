07b2a2dfe62fddbb63531c00b1d1ebfdaad564f2
m-yoshida <m-yoshida@shinsankenkai.com>
2023-04-15 01:56:01 +0000

    created user

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,8 +4,24 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class User extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'department_id',
+        'login_name',
+        'password',
+        'last_name',
+        'first_name',
+        'authority',
+        'email_address',
+    ];
+
+    public function department()
+    {
+        return $this->belongsTo(Department::class);
+    }
 }
--- database/migrations/2023_04_15_002904_create_users_table.php
+++ database/migrations/2023_04_15_002904_create_users_table.php
@@ -4,16 +4,21 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateUsersTable extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
+    public function up()
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('department_id')->constrained()->onDelete('cascade');
+            $table->string('login_name')->unique();
+            $table->string('password');
+            $table->string('last_name');
+            $table->string('first_name');
+            $table->string('authority');
+            $table->string('email_address')->nullable();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
@@ -24,4 +29,5 @@ return new class extends Migration
     {
         Schema::dropIfExists('users');
     }
-};
+}
+;
