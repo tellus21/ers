7d1a3f75cb11e9162be5b492006a1ca03292d40e
m-yoshida <m-yoshida@shinsankenkai.com>
2023-04-29 08:14:17 +0000

    created selected Patient

--- frontend/pages/requests/SearchPatientModal.tsx
+++ frontend/pages/requests/SearchPatientModal.tsx
@@ -30,14 +30,15 @@ export function SearchPatientModal({
     const { createNewDataMutation: createRequest } = useMutateBase('requests')
     // 患者情報を取得するためのhook
     const { query: patients, columns } = usePatientFeature()
+
     // 選択された患者を保持するstate
-    const [patient, setPatient] = useState({ id: '' })
+    const [selectedPatient, setSelectedPatient] = useState({ id: undefined })
 
     // テーブルの行がクリックされたときの処理
     const onRowClick = (rowData: any) => {
         const patientId = rowData.id
         const newPatient: any = filterById(patients, patientId)
-        setPatient(newPatient)
+        setSelectedPatient(newPatient)
     }
 
     // 検査作成ボタンがクリックされたときの処理
@@ -45,9 +46,8 @@ export function SearchPatientModal({
         //依頼を新規登録
         axios
             .post('http://localhost:8000/api/requests', {
-                id: 0,
                 user_id: 1, //ログイン中のユーザid
-                patient_id: patient.id,
+                patient_id: selectedPatient.id,
                 created_at: new Date(),
                 updated_at: new Date(),
                 deleted_at: null,
@@ -66,6 +66,8 @@ export function SearchPatientModal({
         createRequestModalOpen()
     }
 
+    //onCloseの時にdisplayPatientを初期値に戻す関数を作成する
+
     return (
         <Modal opened={opened} onClose={close} title="患者検索" size="70%">
             <Group position="apart">
@@ -75,11 +77,15 @@ export function SearchPatientModal({
 
                 <Text size="sm">選択された患者：</Text>
                 <Text size="sm">
-                    {patient.last_name}　{patient.first_name}さん
+                    {selectedPatient.last_name}　{selectedPatient.first_name}
+                    さん
                 </Text>
-                <Button size="sm" onClick={onCreateButtonClick}>
-                    検査依頼作成
-                </Button>
+
+                {selectedPatient.id && (
+                    <Button size="sm" onClick={onCreateButtonClick}>
+                        検査依頼作成
+                    </Button>
+                )}
             </Group>
 
             <DataTableBase
