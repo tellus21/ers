5faa061932643e0677e36bfff29897ec209cae80
m-yoshida <m-yoshida@shinsankenkai.com>
2023-05-11 00:49:24 +0000

    created editRequest init

--- frontend/pages/requests/index.tsx
+++ frontend/pages/requests/index.tsx
@@ -6,6 +6,9 @@ import { CreateRequestModal } from './request/CreateRequestModal'
 import { useRequestsIndex } from './useRequestsIndex'
 import { Request } from './requestFeature'
 import { Condition } from './condition/conditionFeature'
+import { Insurance } from './insurance/insuranceFeature'
+import { Instruction } from './instruction/instractionFeature'
+import { Appointment } from './appointment/appointmentFeature'
 
 // ReactのContext APIを使用して、編集中の依頼と指示を保持するためのコンテキストを作成
 export const EditedRequestContext = createContext({})
@@ -17,7 +20,27 @@ export default function Index() {
         logicalName,
         columns,
         requests,
+        editedRequest,
         setEditedRequest,
+        patients,
+        displayPatient,
+        setDisplayPatient,
+        conditions,
+        conditionForm,
+        editedCondition,
+        setEditedCondition,
+        insurances,
+        insuranceForm,
+        editedInsurance,
+        setEditedInsurance,
+        instructions,
+        instructionForm,
+        editedInstruction,
+        setEditedInstruction,
+        appointments,
+        appointmentForm,
+        editedAppointment,
+        setEditedAppointment,
         createRequestModalOpend,
         createRequestModalHandlers,
         editRequestModalOpend,
@@ -26,16 +49,43 @@ export default function Index() {
 
     // テーブルの行がクリックされた時の処理
     const onTableRowClick = (rowData: any) => {
-        // クリックされた行のデータを取得して、編集中の依頼としてセットする
+        // クリックされた行のデータをInsuranceInsurance依頼としてセットする
         const selectedRequest = requests.find(
             (request: Request) => request.id === rowData.id
         )
         setEditedRequest(selectedRequest)
+        const conditionValues = conditions.find(
+            (condition: Condition) => condition.id === selectedRequest.id
+        )
+        const insuranceValues = insurances.find(
+            (insurance: Insurance) => insurance.id === selectedRequest.id
+        )
+        insuranceForm.setValues(insuranceValues)
+        // insuranceForm.setValues((prev) => ({ ...prev, ...insuranceValues }))
+        console.log('insuranceValues', insuranceValues)
+        console.log('insuranceForm', insuranceForm.values)
+
+        const instructionValues = instructions.find(
+            (instruction: Instruction) => instruction.id === selectedRequest.id
+        )
+        const appointmentValues = appointments.find(
+            (appointment: Appointment) => appointment.id === selectedRequest.id
+        )
+        //各フォームにsetValuesする
+
+        // setEditedCondition(conditionValues)
+        // setEditedInsurance(insuranceValues)
+        // setEditedInstruction(instructionValues)
+        // setEditedAppointment(appointmentValues)
+
+        // const selectedData = query.find((item: any) => item.id === rowData.id)
+        // //birthdayをDate型に変換してからフォームに設定する
+        // form.setValues(convertDateProperty(selectedData, 'birthday'))
 
         //requestのidを取得して、conditionをsetValuesする
         //その際に、一旦取得時のデータを取得して、比較して、差異があれば、だれかデータ更新したっていう通知だす。
-        const selctedCondition = (condition: Condition) => condition.id
-        console.log('cond', selctedCondition)
+        // const selctedCondition = (condition: Condition) => condition.id
+        // console.log('cond', selctedCondition)
         editRequestModalHandlers.open()
     }
 
--- frontend/pages/requests/useRequestsIndex.ts
+++ frontend/pages/requests/useRequestsIndex.ts
@@ -1,20 +1,48 @@
 import { useRequestFeature } from './requestFeature'
 import { useDisclosure } from '@mantine/hooks'
-import { useSetAtom } from 'jotai'
-import { editedRequestAtom } from './contexts/requestContexts'
+import { useAtom, useSetAtom } from 'jotai'
+import {
+    displayPatientAtom,
+    editedAppointmentAtom,
+    editedConditionAtom,
+    editedInstructionAtom,
+    editedInsuranceAtom,
+    editedRequestAtom,
+} from './contexts/requestContexts'
 import { useConditionFeature } from './condition/conditionFeature'
+import { usePatientFeature } from '../patients/patientFeature'
+import { useInsuranceFeature } from './insurance/insuranceFeature'
+import { useInstructionFeature } from './instruction/instractionFeature'
+import { useAppointmentFeature } from './appointment/appointmentFeature'
 
 // Requestのindexを使用するためのカスタムフック
 export const useRequestsIndex = () => {
     // Requestから論理名、列、クエリを取得
     const { logicalName, columns, query: requests } = useRequestFeature()
-
     // 編集中のRequestを変更する関数を取得
-    const setEditedRequest = useSetAtom(editedRequestAtom)
+    // const setEditedRequest = useSetAtom(editedRequestAtom)
+    const [editedRequest, setEditedRequest] = useAtom(editedRequestAtom)
 
-    //ここからやる！！！！
-    // //Conditionを取得する
-    // const { query: conditions } = useConditionFeature()
+    const { query: patients } = usePatientFeature()
+    const [displayPatient, setDisplayPatient] = useAtom(displayPatientAtom)
+
+    const { query: conditions, form: conditionForm } = useConditionFeature()
+    const [editedCondition, setEditedCondition] = useAtom(editedConditionAtom)
+
+    const { query: insurances, form: insuranceForm } = useInsuranceFeature()
+    const [editedInsurance, setEditedInsurance] = useAtom(editedInsuranceAtom)
+
+    const { query: instructions, form: instructionForm } =
+        useInstructionFeature()
+    const [editedInstruction, setEditedInstruction] = useAtom(
+        editedInstructionAtom
+    )
+
+    const { query: appointments, form: appointmentForm } =
+        useAppointmentFeature()
+    const [editedAppointment, setEditedAppointment] = useAtom(
+        editedAppointmentAtom
+    )
 
     // Request作成モーダルを開閉するための状態を保持
     const [createRequestModalOpend, createRequestModalHandlers] =
@@ -28,7 +56,27 @@ export const useRequestsIndex = () => {
         logicalName,
         columns,
         requests,
+        editedRequest,
         setEditedRequest,
+        patients,
+        displayPatient,
+        setDisplayPatient,
+        conditions,
+        conditionForm,
+        editedCondition,
+        setEditedCondition,
+        insurances,
+        insuranceForm,
+        editedInsurance,
+        setEditedInsurance,
+        instructions,
+        instructionForm,
+        editedInstruction,
+        setEditedInstruction,
+        appointments,
+        appointmentForm,
+        editedAppointment,
+        setEditedAppointment,
         createRequestModalOpend,
         createRequestModalHandlers,
         editRequestModalOpend,
